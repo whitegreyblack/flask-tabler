80bd52471d13cbefb9e624bd95dbd39ec1030795
Sam Whang <sangwoowhang@gmail.com>
2018-04-20 11:43:20 -0500

    Base template working

--- src/index.py
+++ src/index.py
@@ -1,5 +1,29 @@
+import yaml
+from os import walk
+from os.path import splitext
 from flask import Flask, jsonify, render_template
+
+data_path = './data/'
+
 app = Flask(__name__)
+app.config['base'] = '.'
+app.config['data'] = dict()
+
+def parse_yaml_data(path, filename):
+    with open(path + filename) as yaml_file:
+        base_filename = splitext(filename)[0]
+        file_contents = yaml.load(yaml_file)
+        return base_filename, file_contents
+
+# config file
+config_filename, config_data = parse_yaml_data('./', 'config.yml')
+app.config['data'][config_filename] = config_data
+
+# data files
+for _, _, files in walk(data_path):
+    for f in files:
+        filename, data = parse_yaml_data(data_path, f)
+        app.config['data'][filename] = data
 
 @app.route("/")
 @app.route("/index")
@@ -11,21 +35,49 @@ def index():
 def layout():
     return render_template('layout.html', numbers=numbers())
 
+@app.route("/400")
+def error_bad_request():
+    return render_template('400.html')
+
+@app.route("/401")
+def error_authorization():
+    return render_template('401.html')
+
+@app.route("/403")
+def error_forbidden():
+    return render_template('403.html')
+    
 @app.route("/404")
-def error404():
+def error_not_found():
     return render_template('404.html')
 
+@app.route("/410")
+def error_gone():
+    return render_template('410.html')
+
 @app.route('/500')
-def error500():
+def error_internal_server():
     return render_template('500.html')
 
-@app.route("/header")
+@app.route('/503')
+def error_service_unavailable():
+    return render_template('503.html')
+
+@app.route("/demo") 
 def header():
     return render_template('header.html')
 
 @app.route("/numbers")
 def numbers():
     return jsonify({'0': 4, '1': 5, '2': 6})
+
+@app.route("/base")
+def base():
+    page = {
+        'title': 'base',
+        'body_class' : '',
+    }
+    return render_template('base.html', site=app.config, page=page, content='Working?')
 
 if __name__ == '__main__':
     app.run()
